7aeb072ac87506a21912a3ed86eb218b6b8b3fda
Chino Kim <chjno.k@gmail.com>
2016-10-10 14:49:16 -0400

    working text & image tweet

--- walkbot/index.js
+++ walkbot/index.js
@@ -2,17 +2,21 @@ var creds = require('./creds.js');
 var gmap = require('@google/maps').createClient({
   key: creds.g
 });
+var webshot = require('webshot');
+var twit = require('twit');
+var T = new twit(creds.t);
+var fs = require('fs');
 
 
 
 var here = {
-  coords: [39.967627, -98.042574],
+  coords: [],
 };
 
 var there = {
-  coords: [],
+  coords: [39.967627, -98.042574],
   route: {},
-  // time:
+  // localTime: ,
 
   newCoords: function(location){
     var latMin = 24.544090;
@@ -34,12 +38,13 @@ var there = {
     }
 
     this.coords = [newLat, newLong];
+    console.log(this.coords);
   }
 };
 
 
 
-getRoute = function(start, end){
+var getRoute = function(start, end){
   gmap.directions({
     origin: start.coords,
     destination: end.coords,
@@ -51,20 +56,57 @@ getRoute = function(start, end){
 
       there.route.name = data.summary;
       there.route.distance = data.legs[0].distance.text;
-      there.route.duration = data.legs[0].duration.text;
-      there.route.endAddress = data.legs[0].end_address
+      there.route.duration = data.legs[0].duration;
+        // there.route.duration.text
+        // there.route.duration.value * 10000
+      there.route.endAddress = data.legs[0].end_address;
+        // /,\s{1}(.*),\s{1}([A-Z]{2})\s{1}/
+      there.route.endCoords = data.legs[0].end_location;
 
       console.log(there.route);
+      setTimeout(tweet, there.route.duration.value * 10000);
     }
   });
-}
+};
 
-there.newCoords(here);
-getRoute(here, there);
+// there.newCoords(here);
+// getRoute(here, there);
 
 
 
-getNearby = function(location){
+var getStreetView = function(location){
+  var url = 'https://maps.googleapis.com/maps/api/streetview?' +
+    'size=600x400' +
+    '&location=' + location.coords.join(',') +
+    '&fov=120' +
+    '&key=' + creds.g;
+
+    webshot('https://maps.googleapis.com/maps/api/streetview?size=600x400' +
+      '&location=39.92825776163083,-98.02838466827663&fov=120' +
+      '&key=AIzaSyBbw1DChj5bYB4zDSBhdfmGtZZLL8q3VEg',
+      'pic.png',
+      {
+        shotSize: {
+          width: 600,
+          height: 378
+        }
+      },
+      function(err){
+        if (!err){
+          // tweet('hello world', './pic.png');
+        }
+      }
+    );
+
+  console.log(url);
+};
+
+
+
+getStreetView(there);
+
+
+var getNearby = function(location){
   gmap.placesNearby({
     location: location.coords,
     radius: 5000
@@ -81,9 +123,9 @@ getNearby = function(location){
       }
     }
   });
-}
+};
 
-getNearby(there);
+// getNearby(there);
 
 
 
@@ -106,4 +148,46 @@ getNearby(there);
 //       var formattedTime = hours + ':' + minutes.substr(-2);
 //     }
 //   });
-// }
+// };
+
+
+var tweeted = function(err, data, response){
+  if (err){
+    console.log(err);
+  } else {
+    console.log(data.text);
+  }
+};
+
+var tweet = function(text, image){
+  if (image){
+
+    var b64content = fs.readFileSync(image, { encoding: 'base64' });
+    T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+      // now we can assign alt text to the media, for use by screen readers and 
+      // other text-based presentations and interpreters 
+      var mediaIdStr = data.media_id_string;
+      // var altText = "Small flowers in a planter on a sunny balcony, blossoming.";
+      // var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+      var meta_params = { media_id: mediaIdStr};
+     
+      T.post('media/metadata/create', meta_params, function (err, data, response) {
+        if (!err) {
+          var params = { status: text, media_ids: [mediaIdStr] };
+     
+          T.post('statuses/update', params, tweeted);
+        }
+      });
+    });
+
+  } else {
+
+    T.post('statuses/update', {status: text}, tweeted);
+  }
+
+};
+
+
+
+
+
