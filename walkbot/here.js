b8ad0ac637f6727bb7289029eb07cbc14e139f07
Chino Kim <chjno.k@gmail.com>
2016-10-11 03:02:00 -0400

    store here data

---
+++ walkbot/here.js
@@ -0,0 +1,16 @@
+module.exports = { coords: [ 40.1759406, -97.5758218 ],
+  location: 'Hebron, NE',
+  distance: 146074,
+  history: 
+   [ 'ChIJt2rHs9QTmIcROzpPK3-tmZs',
+     'ChIJQTMmTQLZlocRZp_0Vprq84M',
+     'ChIJtzg5mv0PmIcRv5xaHi0uHhE',
+     'ChIJD8HIFZYGmIcRTj0wjRbNjqA',
+     'ChIJV4Kw9ZcGmIcRWwDqNfNbdDA',
+     'ChIJIcJpl70GmIcRMxfnmFRIDUg',
+     'ChIJCbW-CkUYmIcRfeB5xQTcl8I',
+     'ChIJ9ZqS5maJl4cRFjTyx74jguc',
+     'ChIJEU0k2y-Pl4cR-JUf-sr3knc',
+     'ChIJv8IULvKal4cRuiKXPWzAhso',
+     'ChIJybFw3LiRl4cR8hdJxE7gl2I',
+     'ChIJyyGsH7aRl4cRqA4iIBo8wco' ] }
