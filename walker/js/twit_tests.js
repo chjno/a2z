a6a716d5b735c1e16b6ee16fce538fbba7e91e83
Chino Kim <chjno.k@gmail.com>
2016-11-08 12:14:48 -0500

    fix delete tweets

--- walker/js/twit_tests.js
+++ walker/js/twit_tests.js
@@ -1,7 +1,5 @@
-var jimp = require('jimp');
-
 var fs = require('fs');
-var creds = require('./creds.js');
+var creds = require('./js/creds.js');
 var twit = require('twit');
 var T = new twit(creds.t);
 var jsonfile = require('jsonfile');
@@ -56,9 +54,11 @@ var getUser = function(user){
 
 };
 
+
+
 var getTweets = function(user){
 
-  T.get('statuses/user_timeline', {screen_name: user}, function (err, data, response){
+  T.get('statuses/user_timeline', {screen_name: user, count: 200}, function (err, data, response){
     if (!err){
       for (var i = 0; i < data.length; i++){
         console.log(data[i].id_str);
@@ -71,7 +71,7 @@ var getTweets = function(user){
 var destroyTweets = function(tweetID){
   T.post('statuses/destroy/' + tweetID, function (err, data, response){
     if (!err){
-      console.log('tweet destroyed');
+      console.log('tweet ' + tweetID + ' destroyed');
     }
   });
 };
@@ -140,8 +140,62 @@ var updateProfileImage = function(image){
 // updateStatus('hi', './header-small.png');
 // updateProfileImage('./header.png');
 
-jimp.read('header.png', function(err, image){
-  if (err) throw err;
-  var raw = image.bitmap.data;
-  console.log(raw);
-});
+// var tweets = [
+// '793487602473140225',
+// '791882570434764800',
+// '791882266280660992',
+// '791876347794776064',
+// '791872622187470848',
+// '791869786615607296',
+// '791733670000390144',
+// '791678441024258048',
+// '791678045916655616',
+// '791676628904665089',
+// '791675576746729472',
+// '791672448882208768',
+// '791671378990759936',
+// '791671080855429120',
+// '791670746879778816',
+// '791670075426234372',
+// '791667488203341824',
+// '790809284821082112',
+// '790805617610985472',
+// '790805284528742400',
+// '790804012593516544',
+// '790802872409657345',
+// '790801625275310080',
+// '790800794727612416',
+// '790800037517418497',
+// '790798969538486272',
+// '790777742606434304',
+// '790777429010907136',
+// '790777133475954688',
+// '785757365106774016',
+// '785756426765557760',
+// '785754931278012416',
+// '785749949678059520',
+// '785748565725483008',
+// '785742852001234944',
+// '785736388692078592',
+// '785733657017323520',
+// '785732195080101888',
+// '785730619850489856',
+// '785730433770192896',
+// '785730147152453632',
+// '785729976985391104',
+// '785723042366361600',
+// '785721210344710144',
+// '785721016718856192',
+// '785720655480295424',
+// '785720472998649856',
+// '785720312126140416',
+// '785720124112273408',
+// '785719929492344832',
+// '785716295807430656'
+// ];
+
+// for (var i = 0; i < tweets.length; i++){
+//   destroyTweets(tweets[i]);
+// }
+
+// getTweets(me);
